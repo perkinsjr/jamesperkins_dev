1a0913816ee1ebd7e1990b5aba5f3dcd7371f7eb
James Perkins <jamesperkins@hey.com>
2021-03-26 17:35:50 -0400

    Adding some more changes.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import { Chakra } from "../utils/chakra";
 import "../styles/globals.css";
+import theme from "../utils/theme";
 import Header from "../components/header";
+import { ChakraProvider } from "@chakra-ui/react";
 function MyApp({ Component, pageProps }) {
   return (
-    <Chakra cookies={pageProps.cookies}>
+    <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
-    </Chakra>
+    </ChakraProvider>
   );
 }
 export default MyApp;
--- utils/theme.js
+++ utils/theme.js
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 // 2. Add your color mode config
 const config = {
   initialColorMode: "light",
-  useSystemColorMode: true,
+  useSystemColorMode: false,
 };
 
 const colors = {
