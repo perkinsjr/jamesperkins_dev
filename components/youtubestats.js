abde7b3da27b6a3f311691fcb380e6cda3bd2266
James Perkins <jamesperkins@hey.com>
2021-04-18 15:50:49 -0400

    lets just use regex.

--- components/youtubestats.js
+++ components/youtubestats.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Stat, StatLabel, StatNumber } from "@chakra-ui/react";
 export default function YouTubeStat({ label, number }) {
+
+  function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
   return (
     <Stat>
       <StatLabel textAlign="center">{label}</StatLabel>
-      <StatNumber textAlign="center">{number.toLocaleString()}</StatNumber>
+      <StatNumber textAlign="center">{formatNumber(num)}</StatNumber>
     </Stat>
   );
 }
