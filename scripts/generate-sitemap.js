ee043010632e6520ae1ccf50646efc679f004cd9
James Perkins <jamesperkins@hey.com>
2021-04-24 20:29:32 -0400

    fixing sitemap you fool

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -25,7 +25,7 @@ const prettier = require('prettier');
         const route = path === '/index' ? '' : path;
         return `
                         <url>
-                            <loc>${`https://leerob.io${route}`}</loc>
+                            <loc>${`https://jamesperkins.dev${route}`}</loc>
                         </url>
                     `;
       })
