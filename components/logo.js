73e2af8941b9ea80a8b3df8ac0921cfd39a2c9d3
James Perkins <jamesperkins@hey.com>
2021-07-04 18:23:56 -0400

    Removing unused stuff.

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Box, Stack, useColorMode, IconButton } from "@chakra-ui/react";
-import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import { Box, Stack } from "@chakra-ui/react";
 
 export default function Logo(props) {
-  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Box {...props}>
       <Stack mx={8} isInline>
