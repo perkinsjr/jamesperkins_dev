cabc6c58a99944fa91369da66f107c5b224f2777
James Perkins <jamesperkins@hey.com>
2021-08-02 14:19:14 -0400

    sitcky it

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,28 @@
-import { Box, Stack } from '@chakra-ui/react'
-import * as React from 'react'
-import { Copyright } from './Copyright'
-import { Logo } from './Logo'
-import { SocialMediaLinks } from './SocialMediaLinks'
+import { Box, Stack } from '@chakra-ui/react';
+import * as React from 'react';
+import { Copyright } from './Copyright';
+import { Logo } from './Logo';
+import { SocialMediaLinks } from './SocialMediaLinks';
 
 const Footer = () => {
-return(
-  <Box as="footer" role="contentinfo" mx="auto" maxW="7xl" py="4" px={{ base: '4', md: '8' }}>
-    <Stack>
-      <Stack direction="row" spacing="4" align="center" justify="space-between">
-        <Logo />
-        <SocialMediaLinks />
-      </Stack>
-      <Copyright alignSelf={{ base: 'center', sm: 'start' }} />
-    </Stack>
-  </Box>
-    )
-}
+    return (
+        <Box
+            as="footer"
+            position="sticky"
+            role="contentinfo"
+            mx="auto"
+            maxW="7xl"
+            py="4"
+            px={{ base: '4', md: '8' }}>
+            <Stack>
+                <Stack direction="row" spacing="4" align="center" justify="space-between">
+                    <Logo />
+                    <SocialMediaLinks />
+                </Stack>
+                <Copyright alignSelf={{ base: 'center', sm: 'start' }} />
+            </Stack>
+        </Box>
+    );
+};
 
 export default Footer;
