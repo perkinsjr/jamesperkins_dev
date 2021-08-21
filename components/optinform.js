c0e5c8e27b1f36b23d47656b3d195e060f2a4a73
James Perkins <jamesperkins@hey.com>
2021-08-21 10:57:54 -0400

    Adding new optin

--- components/optinform.js
+++ components/optinform.js
@@ -1,4 +1,3 @@
-import React, { useState, useRef } from "react";
 import SuccessMessage from "./successmessage";
 import ErrorMessage from "./errormessage";
 import {
@@ -12,38 +11,6 @@ import StyledButton from "./button";
 
 
 const OptInForm = () => {
-  const [form, setForm] = useState(false);
-  const inputEl = useRef(null);
-
-  const subscribe = async (e) => {
-    e.preventDefault();
-    setForm({ state: "loading" });
-
-    const res = await fetch("/api/subscribe", {
-      body: JSON.stringify({
-        email: inputEl.current.value,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    });
-
-    const { error } = await res.json();
-    if (error) {
-      setForm({
-        state: "error",
-        message: error,
-      });
-      return;
-    }
-
-    inputEl.current.value = "";
-    setForm({
-      state: "success",
-      message: `Congratulations, you are on the list`,
-    });
-  };
   return (
     <Box as="section"py="6">
       <Box
@@ -61,34 +28,23 @@ const OptInForm = () => {
           <Heading mt="4" fontWeight="extrabold">
             Get new content every week in your inbox!
           </Heading>
-          <Box mt="4">
-            <form onSubmit={subscribe}>
+          <Box mt="4" id="revue-embed">
+          <form action="https://www.getrevue.co/profile/james_r_perkins/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank">
               <Stack>
                 <Input
                   aria-label="Enter your email"
                   placeholder="Enter your email to join"
                   rounded="base"
-                  ref={inputEl}
-          id="email"
-          type="email"
-          name="email"
-                />
+                  name="member[email]" type="email" id="member_email"/>
                 <input type="hidden" value="1" name="embed" />
-                <StyledButton
-          isLoading={form.state === "loading"}
-          type="submit"
+                <StyledButton type="submit"
           text="Sign Up"
-        />
+          value="Subscribe" name="member[subscribe]" id="member_submit"/>
               </Stack>
             </form>
             <Text color='gray.600' fontSize="sm" mt="5">
               I only send you relevant content
             </Text>
-            {form.state === "error" ? (
-        <ErrorMessage>{form.message}</ErrorMessage>
-      ) : (
-        <SuccessMessage>{form.message}</SuccessMessage>
-      )}
           </Box>
         </Box>
       </Box>
